9eb5da5e29accedd5fdf3b5c4d934ae16bf05674
ankitsingh0188 <ankitsingh0188@gmail.com>
2021-05-11 19:49:30 +0530

    LCH-4833: Created platform group traits

---
+++ src/Command/PlatformGroupTrait.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Acquia\Console\Helpers\Command;
+
+use Symfony\Component\Yaml\Exception\ParseException;
+use Symfony\Component\Yaml\Yaml;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Trait PlatformGroupTrait.
+ *
+ * @package Acquia\Console\Helpers\Traits
+ */
+trait PlatformGroupTrait {
+
+  /**
+   * Fetch the location of the platform group file.
+   *
+   * @return string
+   *   Platform group file path.
+   */
+  protected function groupingSitesFilePath(): string {
+    $alias = $this->getAlias();
+    $dir_parts = static::PLATFORM_LOCATION;
+    array_unshift($dir_parts, getenv('HOME'));
+
+    return implode(DIRECTORY_SEPARATOR, $dir_parts) . "/{$alias}.yml";
+  }
+
+  /**
+   * Filter list of sites via group sites.
+   *
+   * @param string $group_name
+   *   Platform grouping sites placeholder.
+   * @param array $sites
+   *   Platform sites.
+   * @param OutputInterface $output
+   *   Output stream.
+   *
+   * @return array
+   *   Array containing filtered list of sites.
+   */
+  protected function filterSitesByGroup(string $group_name, array $sites, OutputInterface $output): array {
+    $group_file = $this->groupingSitesFilePath();
+    try {
+      $group_config = Yaml::parseFile($group_file);
+    }
+    catch (ParseException $exception) {
+      $output->writeln('<error>Unable to parse the YAML ' . $exception->getMessage() . '</error>', );
+      return [];
+    }
+
+    if (!isset($group_config[$group_name])) {
+      $output->writeln('<error>Group name doesn\'t exists.</error>');
+      return [];
+    }
+
+    if (empty($group_config[$group_name])) {
+      $output->writeln('<warning>No sites available in the groups. Exiting...</warning>');
+      return [];
+    }
+
+    return array_intersect_key($sites, array_flip($group_config[$group_name]));
+  }
+
+}
