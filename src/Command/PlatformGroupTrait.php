4bf83c0b4dfdbe3827f56c3ca7ae2eb0ad7eb6be
Ankit Singh <ankit.singh@acquia.com>
2021-11-24 17:23:37 +0530

    LCH-4833: Resolved PR comments (#9)

--- src/Command/PlatformGroupTrait.php
+++ src/Command/PlatformGroupTrait.php
@@ -5,6 +5,8 @@ namespace Acquia\Console\Helpers\Command;
 use Acquia\Console\Acsf\Platform\ACSFPlatform;
 use Acquia\Console\Cloud\Platform\AcquiaCloudMultiSitePlatform;
 use Acquia\Console\Cloud\Platform\AcquiaCloudPlatform;
+use EclipseGc\CommonConsole\PlatformInterface;
+use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Yaml\Exception\ParseException;
 use Symfony\Component\Yaml\Yaml;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -85,6 +87,33 @@ trait PlatformGroupTrait {
     }
 
     return $sites;
+  }
+
+  /**
+   * Gets the uri of one of the sites.
+   *
+   * @param \EclipseGc\CommonConsole\PlatformInterface $platform
+   *   Platform object.
+   * @param \Symfony\Component\Console\Input\InputInterface $input
+   *   Input stream.
+   * @param \Symfony\Component\Console\Output\OutputInterface $output
+   *   Output stream.
+   *
+   * @return string
+   *   URI to return.
+   */
+  protected function getUri(PlatformInterface $platform, InputInterface $input, OutputInterface $output): string {
+    $sites = [];
+    $platform_id = $platform->getPlatformId();
+    $sites = $platform->getPlatformSites();
+    $group_name = $input->hasOption('group') ? $input->getOption('group') : '';
+    if (!empty($group_name)) {
+      $alias = $platform->getAlias();
+      $sites = $this->filterSitesByGroup($group_name, $sites, $output, $alias, $platform_id);
+    }
+
+    $site_info = reset($sites);
+    return $site_info['uri'] ?? $site_info;
   }
 
 }
